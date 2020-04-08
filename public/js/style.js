9f976bdbd2b1ed3d6982ae612b40bf3965470d5b
Matheus <matheusgomes192@hotmail.com>
2020-04-08 17:04:00 -0300

    diminuindo altura do scroll na home

--- public/js/style.js
+++ public/js/style.js
@@ -1,6 +1,6 @@
 $(window).scroll(function(){ // função para pegar o movimento do scroll (barra de rolagem)
     var top = $(window).scrollTop(); // aqui vc pega a posição da página
-    if(top > 250){ // verifica a posição da página
+    if(top > 200){ // verifica a posição da página
         $('#texts').css('visibility', 'visible');
         $("#texts").addClass('fadeInUp'); // aqui vc aplica o fade no menu$
         $('#textSocial').css('visibility', 'visible');
