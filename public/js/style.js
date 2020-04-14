4a74c71dd31d30cfe8c4dce5cc720ced2735a42a
Matheus <matheusgomes192@hotmail.com>
2020-04-14 15:01:01 -0300

    adicionando efeitos de fade na div services

--- public/js/style.js
+++ public/js/style.js
@@ -2,7 +2,7 @@ $(window).scroll(function(){ // função para pegar o movimento do scroll (barra
     var top = $(window).scrollTop(); // aqui vc pega a posição da página
     if(top > 200){ // verifica a posição da página
         $('#texts').css('visibility', 'visible');
-        $("#texts").addClass('fadeInUp'); // aqui vc aplica o fade no menu$
+        $("#texts").addClass('fadeInUp'); // aqui vc aplica o fade no menu
         $('#textSocial').css('visibility', 'visible');
         $("#textSocial").addClass('fadeInUp'); // aqui vc aplica o fade no menu
     }
@@ -13,6 +13,16 @@ $(window).scroll(function(){ // função para pegar o movimento do scroll (barra
     if(top > 350) {
         $('#title').css('visibility', 'visible');
         $('#title').addClass('fadeInUp');
+    }
+    if(top > 800) {
+        $('#servTitle').css('visibility', 'visible');
+        $("#servTitle").addClass('fadeInUp'); // aqui vc aplica o fade no menu
+        $('#servText').css('visibility', 'visible');
+        $("#servText").addClass('fadeInUp'); // aqui vc aplica o fade no menu
+    }
+    if(top > 1100) {
+        $('.content1').css('visibility', 'visible');
+        $(".content1").addClass('fadeInUp'); // aqui vc aplica o fade no menu
     }
 });
 
