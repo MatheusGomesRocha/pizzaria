8316528b483b04632ae089e350a938afb42aa860
Matheus <matheusgomes192@hotmail.com>
2020-05-27 14:12:38 -0300

    mudando style da view cardapio

--- public/js/cardapio.js
+++ public/js/cardapio.js
@@ -1,22 +1,22 @@
 function pizza() {
-    document.getElementById('btnPizzaCardapio').style.backgroundColor = "#fff";
-    document.getElementById('btnPizzaCardapio').style.color = "#000";
-    document.getElementById('btnBurgerCardapio').style.backgroundColor = "#000";
-    document.getElementById('btnBurgerCardapio').style.color = "#fff";
-    document.getElementById('btnDrinkCardapio').style.backgroundColor = "#000";
-    document.getElementById('btnDrinkCardapio').style.color = "#fff";
+    document.getElementById('btnPizzaCardapio').style.backgroundColor = "#F03A47";
+    document.getElementById('btnPizzaCardapio').style.color = "#fff";
+    document.getElementById('btnBurgerCardapio').style.backgroundColor = "transparent";
+    document.getElementById('btnBurgerCardapio').style.color = "#F03A47";
+    document.getElementById('btnDrinkCardapio').style.backgroundColor = "transparent";
+    document.getElementById('btnDrinkCardapio').style.color = "#F03A47";
     document.getElementById('divPaiCardapio').style.display = "block";
     document.getElementById('divPaiCardapio1').style.display = "none";
     document.getElementById('divPaiCardapio2').style.display = "none";
 }
 
 function burger() {
-    document.getElementById('btnPizzaCardapio').style.backgroundColor = "#000";
-    document.getElementById('btnPizzaCardapio').style.color = "#fff";
-    document.getElementById('btnBurgerCardapio').style.backgroundColor = "#fff";
-    document.getElementById('btnBurgerCardapio').style.color = "#000";
-    document.getElementById('btnDrinkCardapio').style.backgroundColor = "#000";
-    document.getElementById('btnDrinkCardapio').style.color = "#fff";
+    document.getElementById('btnPizzaCardapio').style.backgroundColor = "transparent";
+    document.getElementById('btnPizzaCardapio').style.color = "#F03A47";
+    document.getElementById('btnBurgerCardapio').style.backgroundColor = "#F03A47";
+    document.getElementById('btnBurgerCardapio').style.color = "#fff";
+    document.getElementById('btnDrinkCardapio').style.backgroundColor = "transparent";
+    document.getElementById('btnDrinkCardapio').style.color = "#F03A47";
     document.getElementById('divPaiCardapio').style.display = "none";
     document.getElementById('divPaiCardapio1').style.display = "block";
     document.getElementById('divPaiCardapio2').style.display = "none";
@@ -24,12 +24,12 @@ function burger() {
 
 
 function drink() {
-    document.getElementById('btnPizzaCardapio').style.backgroundColor = "#000";
-    document.getElementById('btnPizzaCardapio').style.color = "#fff";
-    document.getElementById('btnBurgerCardapio').style.backgroundColor = "#000";
-    document.getElementById('btnBurgerCardapio').style.color = "#fff";
-    document.getElementById('btnDrinkCardapio').style.backgroundColor = "#fff";
-    document.getElementById('btnDrinkCardapio').style.color = "#000";
+    document.getElementById('btnPizzaCardapio').style.backgroundColor = "transparent";
+    document.getElementById('btnPizzaCardapio').style.color = "#F03A47";
+    document.getElementById('btnBurgerCardapio').style.backgroundColor = "transparent";
+    document.getElementById('btnBurgerCardapio').style.color = "#F03A47";
+    document.getElementById('btnDrinkCardapio').style.backgroundColor = "#F03A47";
+    document.getElementById('btnDrinkCardapio').style.color = "#fff";
     document.getElementById('divPaiCardapio').style.display = "none";
     document.getElementById('divPaiCardapio1').style.display = "none";
     document.getElementById('divPaiCardapio2').style.display = "block";
